8807c1e6824d7779ab5832e864ed5b3b630ca530
pumbaa666 <pumbaa666@gmail.com>
2022-12-06 13:20:56 +0100

    Day 06 : Done

--- src/ch/correvon/adventofcode/day06/Main_Day06.java
+++ src/ch/correvon/adventofcode/day06/Main_Day06.java
@@ -4,13 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main_Day06
 {
 	public static void main(String[] args)
 	{
-		int result = parseFile(getFileStream("data.txt"), false);
-		System.out.println("Result : " + result);
+		int result = parseFile(getFileStream("data.txt"), 4);
+		System.out.println("Last result Part A : " + result);
+		// Last result : 1198
+
+		result = parseFile(getFileStream("data.txt"), 14);
+		System.out.println("Result Part B: " + result);
+		// Last result : 3120
 	}
 
 	private static InputStream getFileStream(String fileName)
@@ -21,21 +31,31 @@ public class Main_Day06
 		return inputStream;
 	}
 
-	private static int parseFile(InputStream inputStream, boolean partB)
+	private static int parseFile(InputStream inputStream, int bufferSize)
 	{
+		int i = 0;
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream)))
 		{
 			String line;
 			int lineNum = 0;
-			while((line = br.readLine()) != null)
+			line:while((line = br.readLine()) != null)
 			{
-				String[] split = line.split(" ");
-				if(split.length != 2)
-				{
-					System.err.println("Invalid line " + lineNum + " : " + line);
-					continue;
-				}
+				System.out.println("\nline : " + line);
 				
+				i = 1;
+				Character[] buffer = new Character[bufferSize];
+				for(char c:line.toCharArray())
+				{
+					buffer[i%bufferSize] = c;
+					if(checkBufferUnicity(buffer, bufferSize))
+					{
+					    Stream<Character> charStream = Arrays.stream(buffer);
+					    String bufferEnString = charStream.map(String::valueOf).collect(Collectors.joining());
+						System.out.println("1st uniques at index "+i+" : " + bufferEnString);
+						continue line;
+					}
+					i++;
+				}
 
 				lineNum++;
 			}
@@ -46,6 +66,21 @@ public class Main_Day06
 			e.printStackTrace();
 		}
 		
-		return -1;
+		return i;
 	}
+	
+	public static boolean checkBufferUnicity(Character[] arr, int minimalBufferSize)
+    {
+		if(arr.length < minimalBufferSize)
+			return false;
+		for(int i = 0; i < minimalBufferSize; i++)
+			if(arr[i] ==  null)
+				return false;
+		
+        // Put all array elements in a HashSet
+		Set<Character> s = new HashSet<Character>(Arrays.asList(arr));
+ 
+        // If all elements are distinct, size of HashSet should be same array.
+        return (s.size() == arr.length);
+    }
 }
